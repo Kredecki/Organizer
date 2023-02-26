121deea5906aa3c22c33a03c870a6bf2347a86d6
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-02-26 19:49:37 +0100

    Update HomeRepository.cs

--- Data/Repositories/HomeRepository.cs
+++ Data/Repositories/HomeRepository.cs
@@ -19,93 +19,43 @@ namespace Organizer.Data.Repositories
     public class HomeRepository : IHomeRepository
     {
         private readonly ApplicationDbContext _db;
-        private readonly IHomeService _homeService;
 
-        public HomeRepository(ApplicationDbContext db, IHomeService homeService)
+        public HomeRepository(ApplicationDbContext db)
         {
             _db = db;
-            _homeService = homeService;
         }
 
         public async Task<List<TodoItem>> GetAllTodosList(int page, int itemsOnPage)
         {
-            try 
-            { 
-                return await _db.TodoItem.Skip((page - 1) * itemsOnPage).Take(itemsOnPage).ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                _homeService.HandleException(ex, "Failed to get all todo items from database");
-                throw;
-            }
+            return await _db.TodoItem.Skip((page - 1) * itemsOnPage).Take(itemsOnPage).ToListAsync();
         }
         
         public async Task<int> CountAllTodos()
         {
-            try
-            {
-                return await _db.TodoItem.CountAsync();
-            }
-            catch (Exception ex)
-            {
-                _homeService.HandleException(ex, "Failed to count all todo items from database");
-                throw;
-            }
+            return await _db.TodoItem.CountAsync();
         }
 
         public async Task<TodoItem?> GetTodoById(int id)
         {
-            try
-            {
-                return await _db.TodoItem.FindAsync(id);
-            }
-            catch (Exception ex)
-            {
-                _homeService.HandleException(ex, "Failed to get todo items from database");
-                throw;
-            }
+            return await _db.TodoItem.FindAsync(id);
         }
 
         public async Task AddTodoItem(TodoItem todo)
         {
-            try
-            {
-                await _db.TodoItem.AddAsync(todo);
-                await _db.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                _homeService.HandleException(ex, "Failed to add todo item to database");
-                throw;
-            }
+            await _db.TodoItem.AddAsync(todo);
+            await _db.SaveChangesAsync();
         }
 
         public async Task UpdateTodoItem(TodoItem todo)
         {
-            try
-            {
-                _db.TodoItem.Update(todo);
-                await _db.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                _homeService.HandleException(ex, "Failed to update todo item into database");
-                throw;
-            }
+            _db.TodoItem.Update(todo);
+            await _db.SaveChangesAsync();
         }
 
         public async Task DeleteTodoItem(TodoItem todo)
         {
-            try
-            {
-                _db.TodoItem.Remove(todo);
-                await _db.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                _homeService.HandleException(ex, "Failed to delete todo item from database");
-                throw;
-            }
+            _db.TodoItem.Remove(todo);
+            await _db.SaveChangesAsync();
         }
     }
 }
